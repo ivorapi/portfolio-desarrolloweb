592c8ec3aec8bdc1892220c069725113f5bd9c3a
Ivan <ivrapisarda@uade.edu.ar>
2024-11-08 02:54:27 -0300

    darkmode

--- botonup.js
+++ botonup.js
@@ -24,3 +24,32 @@ function toggleScrollToTopButton() {
 
   window.addEventListener("scroll", toggleScrollToTopButton);
   document.getElementById("scrollToTopBtn").addEventListener("click", scrollToTop);
+
+
+    // Función para cambiar entre modo claro y oscuro
+    function toggleDarkMode() {
+        const body = document.body;
+        const button = document.getElementById('dark-mode-toggle');
+        
+        body.classList.toggle('dark-mode');
+        
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+            button.innerHTML = '☀️';
+            button.setAttribute('aria-label', 'Cambiar a modo claro');
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+            button.innerHTML = '🌙';
+            button.setAttribute('aria-label', 'Cambiar a modo oscuro');
+        }
+    }
+
+    // Verificar la preferencia guardada del usuario
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark-mode');
+        document.getElementById('dark-mode-toggle').innerHTML = '☀️';
+        document.getElementById('dark-mode-toggle').setAttribute('aria-label', 'Cambiar a modo claro');
+    }
+
+    // Agregar evento de clic al botón
+    document.getElementById('dark-mode-toggle').addEventListener('click', toggleDarkMode);
